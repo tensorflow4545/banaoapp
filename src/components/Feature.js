9c9d59c33cb38dd35310df36e8b961657e7fb839
tensorflow4545 <anshulojha1214@gmail.com>
2024-06-24 21:38:09 +0530

    chnages

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -6,28 +6,28 @@ import feature4 from "@/assets/feature4.png";
 export default function Feature(){
     const features=[
         {
-            key:1,
+            id:1,
             head:"People",
             desc:"Find a teacher, coach, or expert for your hobby interest in your locality.  Find a partner, teammate, accompanist or collaborator.",
             buttontext:"Connect",
             img:feature1
         },
         {
-            key:2,
+            id:2,
             head:"Place",
             desc:"Find a class, school, playground, auditorium, studio, shop or an event venue.  Book a slot at venues that allow booking through hobbycue.",
             buttontext:"Meet up",
             img:feature2
         },
         {
-            key:3,
+            id:3,
             head:"Product",
             desc:"Find equipment or supplies required for your hobby.  Buy, rent or borrow from shops, online stores or from community members.",
             buttontext:"Get it",
             img:feature3
         },
         {
-            key:4,
+            id:4,
             head:"Program",
             desc:"Find events, meetups and workshops related to your hobby.  Register or buy tickets online.",
             buttontext:"Attend",
@@ -38,8 +38,8 @@ export default function Feature(){
         <>
            <div className="w-full h-max lg:px-[100px] md:px-[60px] px-[50px] lg:py-[100px] md:py-[40px] py-[20px] bg-white">
              <div className="lg:grid md:grid lg:grid-cols-2 md:grid-cols-2 gap-[10px]">
-             {features.map((element)=>(
-                <div className="max-w-[608px] h-max px-[30px] py-[20px] lg:mb-[0px] mb-[20px] rounded-md border-[2px] border-[#94a3b8]">
+             {features.map((element,index)=>(
+                <div key={index} className="max-w-[608px] h-max px-[30px] py-[20px] lg:mb-[0px] mb-[20px] rounded-md border-[2px] border-[#94a3b8]">
                    <div className="flex gap-[40px] mb-[25px]">
                      <div className="w-[40px] h-[40px] relative"><Image src={element.img} fill alt="Image"></Image></div>
                      <p className="text-[24px] font-bold">{element.head}</p>
